d6a22fd494bd9b987ef1d5c38ca84d5bd2d17141
Josh Dolitsky <jdolitsky@gmail.com>
2018-12-28 04:14:34 -0600

    use custom mediatype in example

--- examples/simple_push_pull.go
+++ examples/simple_push_pull.go
@@ -19,13 +19,17 @@ func main() {
 	ref := "localhost:5000/oras:test"
 	fileName := "hello.txt"
 	fileContent := []byte("Hello World!\n")
+	customMediaType := "my.custom.media.type"
 
 	ctx := context.Background()
 	resolver := docker.NewResolver(docker.ResolverOptions{})
 
-	// Push file(s) to registry
+	// Push file(s) w custom mediatype to registry
 	pushContents := make(map[string]oras.Blob)
-	pushContents[fileName] = oras.Blob{Content: fileContent}
+	pushContents[fileName] = oras.Blob{
+		Content: fileContent,
+		MediaType: customMediaType,
+	}
 	fmt.Printf("Pushing %s to %s... ", fileName, ref)
 	err := oras.Push(ctx, resolver, ref, pushContents)
 	check(err)
@@ -33,7 +37,8 @@ func main() {
 
 	// Pull file(s) from registry and save to disk
 	fmt.Printf("Pulling from %s and saving to %s... ", ref, fileName)
-	pullContents, err := oras.Pull(ctx, resolver, ref)
+	allowedMediaTypes := []string{customMediaType}
+	pullContents, err := oras.Pull(ctx, resolver, ref, allowedMediaTypes...)
 	check(err)
 	err = ioutil.WriteFile(fileName, pullContents[fileName].Content, 0644)
 	check(err)
