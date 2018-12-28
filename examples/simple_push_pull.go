fcbeea3136aaa93e896a2ad2d6cdca34c2939abe
Josh Dolitsky <jdolitsky@gmail.com>
2018-12-28 04:08:13 -0600

    fix example

--- examples/simple_push_pull.go
+++ examples/simple_push_pull.go
@@ -3,9 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+
 	"github.com/containerd/containerd/remotes/docker"
 	"github.com/shizhMSFT/oras/pkg/oras"
-	"io/ioutil"
 )
 
 func check(e error) {
@@ -23,8 +24,8 @@ func main() {
 	resolver := docker.NewResolver(docker.ResolverOptions{})
 
 	// Push file(s) to registry
-	pushContents := make(map[string][]byte)
-	pushContents[fileName] = fileContent
+	pushContents := make(map[string]oras.Blob)
+	pushContents[fileName] = oras.Blob{Content: fileContent}
 	fmt.Printf("Pushing %s to %s... ", fileName, ref)
 	err := oras.Push(ctx, resolver, ref, pushContents)
 	check(err)
@@ -34,7 +35,7 @@ func main() {
 	fmt.Printf("Pulling from %s and saving to %s... ", ref, fileName)
 	pullContents, err := oras.Pull(ctx, resolver, ref)
 	check(err)
-	err = ioutil.WriteFile(fileName, pullContents[fileName], 0644)
+	err = ioutil.WriteFile(fileName, pullContents[fileName].Content, 0644)
 	check(err)
 	fmt.Println("success!")
 	fmt.Printf("Try running 'cat %s'\n", fileName)
