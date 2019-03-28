a6d046183517b636143e22e0e58f72b7c8436519
Shiwei Zhang <shizh@microsoft.com>
2019-03-28 14:56:53 +0800

    change parameter names

--- pkg/auth/client.go
+++ pkg/auth/client.go
@@ -8,10 +8,10 @@ import (
 
 // Client provides authentication operations for remotes.
 type Client interface {
-	// Login logs in to a remote server identified by the server address.
-	Login(ctx context.Context, serverAddress, username, secret string) error
-	// Logout logs out from a remote server identified by the server address.
-	Logout(ctx context.Context, serverAddress string) error
+	// Login logs in to a remote server identified by the hostname.
+	Login(ctx context.Context, hostname, username, secret string) error
+	// Logout logs out from a remote server identified by the hostname.
+	Logout(ctx context.Context, hostname string) error
 	// Resolver returns a new authenticated resolver.
 	Resolver(ctx context.Context) (remotes.Resolver, error)
 }
