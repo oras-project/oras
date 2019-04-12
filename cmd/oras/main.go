f39cb536b4a69c6d879306f7200c3f957168fba4
Shiwei Zhang <shizh@microsoft.com>
2019-04-12 15:04:57 +0800

    Add version

--- cmd/oras/main.go
+++ cmd/oras/main.go
@@ -10,6 +10,7 @@ func main() {
 	cmd := &cobra.Command{
 		Use:          "oras [command]",
 		SilenceUsage: true,
+		Version:      "v0.4.0",
 	}
 	cmd.AddCommand(pullCmd(), pushCmd(), loginCmd(), logoutCmd())
 	if err := cmd.Execute(); err != nil {
