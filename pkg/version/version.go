a48d475531e44641a30ea8b980432c7149e7142e
Shiwei Zhang <shizh@microsoft.com>
2019-04-15 16:42:10 +0800

    validate comments grammar

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	// Version is the current version of the oras.
 	Version = "0.4.0"
-	// BuildMetadata is extra build time data
+	// BuildMetadata is the extra build time data
 	BuildMetadata = "unreleased"
 	// GitCommit is the git sha1
 	GitCommit = ""
