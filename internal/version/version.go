bf2ff5d15c39eee127c14f4b5b83f50bf60346fe
Shiwei Zhang <shizh@microsoft.com>
2019-04-17 11:31:54 +0800

    oras version (#69)
    support version with makefile, dockerfile, and goreleaser updated

---
+++ internal/version/version.go
@@ -0,0 +1,20 @@
+package version
+
+var (
+	// Version is the current version of the oras.
+	Version = "0.4.0"
+	// BuildMetadata is the extra build time data
+	BuildMetadata = "unreleased"
+	// GitCommit is the git sha1
+	GitCommit = ""
+	// GitTreeState is the state of the git tree
+	GitTreeState = ""
+)
+
+// GetVersion returns the semver string of the version
+func GetVersion() string {
+	if BuildMetadata == "" {
+		return Version
+	}
+	return Version + "+" + BuildMetadata
+}
