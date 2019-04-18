8e01dabb99b5c9e85d58e505ce3712f452ac4645
Shiwei Zhang <shizh@microsoft.com>
2019-04-18 12:46:00 +0800

    Pull in sequence

--- pkg/oras/pull_opts.go
+++ pkg/oras/pull_opts.go
@@ -2,6 +2,7 @@ package oras
 
 type pullOpts struct {
 	allowedMediaTypes []string
+	inSequence        bool
 }
 
 // PullOpt allows callers to set options on the oras pull
@@ -25,4 +26,10 @@ func WithAllowedMediaTypes(allowedMediaTypes []string) PullOpt {
 		o.allowedMediaTypes = append(o.allowedMediaTypes, allowedMediaTypes...)
 		return nil
 	}
+}
+
+// WithPullInSequence opt to pull in sequence with breath-first search
+func WithPullInSequence(o *pullOpts) error {
+	o.inSequence = true
+	return nil
 }
