39de0678ff2995b33cb8c7ba7430042a836737bb
Shiwei Zhang <shizh@microsoft.com>
2019-05-01 02:17:53 +0800

    fix bug (#93)

--- pkg/oras/push.go
+++ pkg/oras/push.go
@@ -65,7 +65,9 @@ func pack(provider content.Provider, descriptors []ocispec.Descriptor, opts *pus
 	} else {
 		config = *opts.config
 	}
-	config.Annotations = opts.configAnnotations
+	if opts.configAnnotations != nil {
+		config.Annotations = opts.configAnnotations
+	}
 
 	// Manifest
 	manifest := ocispec.Manifest{
