ffae2ae0da7a28dda32a8143cd9e545a8aff86ec
Shiwei Zhang <shizh@microsoft.com>
2021-10-28 16:32:48 +0800

    add artifact manifest type to the headers
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

--- cmd/oras/resolver.go
+++ cmd/oras/resolver.go
@@ -6,16 +6,30 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	auth "github.com/deislabs/oras/pkg/auth/docker"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	artifactspec "github.com/oras-project/artifacts-spec/specs-go/v1"
 
+	"github.com/containerd/containerd/images"
 	"github.com/containerd/containerd/remotes"
 	"github.com/containerd/containerd/remotes/docker"
 )
 
 func newResolver(username, password string, insecure bool, plainHTTP bool, configs ...string) (remotes.Resolver, *docker.ResolverOptions) {
+	header := http.Header{}
+	header.Set("Accept", strings.Join([]string{
+		images.MediaTypeDockerSchema2Manifest,
+		images.MediaTypeDockerSchema2ManifestList,
+		ocispec.MediaTypeImageManifest,
+		ocispec.MediaTypeImageIndex,
+		artifactspec.MediaTypeArtifactManifest,
+		"*/*",
+	}, ", "))
 	opts := &docker.ResolverOptions{
 		PlainHTTP: plainHTTP,
+		Headers:   header,
 	}
 
 	client := http.DefaultClient
