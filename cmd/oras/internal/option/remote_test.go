d5696c57b641081c2e33ecdaf9915d4e7cc3c0e8
Billy Zha <jinzha1@microsoft.com>
2022-05-30 22:35:37 +0800

    Add unit tests
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- cmd/oras/internal/option/remote_test.go
+++ cmd/oras/internal/option/remote_test.go
@@ -20,18 +20,29 @@ import (
 	"crypto/rand"
 	"crypto/x509"
 	"encoding/base64"
+	"encoding/json"
 	"encoding/pem"
+	"fmt"
 	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
 
 	nhttp "net/http"
 	"net/http/httptest"
+	"net/url"
 
+	"github.com/spf13/pflag"
 	"oras.land/oras-go/v2/registry/remote/auth"
 )
 
 var ts *httptest.Server
+var testRepo = "test-repo"
+var testTagList = struct {
+	Tags []string `json:"tags"`
+}{
+	Tags: []string{"tag"},
+}
 
 func TestMain(m *testing.M) {
 	// Test server
@@ -41,13 +52,24 @@ func TestMain(m *testing.M) {
 		switch {
 		case p == "/v2/" && m == "GET":
 			w.WriteHeader(nhttp.StatusOK)
+		case p == fmt.Sprintf("/v2/%s/tags/list", testRepo) && m == "GET":
+			json.NewEncoder(w).Encode(testTagList)
 		}
 	}))
 	defer ts.Close()
 	m.Run()
 }
 
-func Test_authClient_RawCredential(t *testing.T) {
+func TestRemote_FlagsInit(t *testing.T) {
+	var test struct {
+		Remote
+	}
+
+	fs := pflag.NewFlagSet("oras-test", pflag.ExitOnError)
+	test.ApplyFlags(fs)
+}
+
+func TestRemote_authClient_RawCredential(t *testing.T) {
 	password := make([]byte, 12)
 	if _, err := rand.Read(password); err != nil {
 		t.Fatalf("unexpected error: %v", err)
@@ -74,7 +96,7 @@ func Test_authClient_RawCredential(t *testing.T) {
 	}
 }
 
-func Test_authClient_skipTlsVerify(t *testing.T) {
+func TestRemote_authClient_skipTlsVerify(t *testing.T) {
 	opts := Remote{
 		Insecure: true,
 	}
@@ -92,7 +114,7 @@ func Test_authClient_skipTlsVerify(t *testing.T) {
 	}
 }
 
-func Test_authClient_CARoots(t *testing.T) {
+func TestRemote_authClient_CARoots(t *testing.T) {
 	caPath := filepath.Join(t.TempDir(), "oras-test.pem")
 	if err := os.WriteFile(caPath, pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: ts.Certificate().Raw}), 0644); err != nil {
 		t.Fatalf("unexpected error: %v", err)
@@ -114,6 +136,73 @@ func Test_authClient_CARoots(t *testing.T) {
 	}
 	_, err = client.Do(req)
 	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
+
+func TestRemote_NewRegistry(t *testing.T) {
+	caPath := filepath.Join(t.TempDir(), "oras-test.pem")
+	if err := os.WriteFile(caPath, pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: ts.Certificate().Raw}), 0644); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	pool := x509.NewCertPool()
+	pool.AddCert(ts.Certificate())
+
+	opts := struct {
+		Remote
+		Common
+	}{
+		Remote{
+			CACertFilePath: caPath,
+		},
+		Common{},
+	}
+	uri, err := url.ParseRequestURI(ts.URL)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	reg, err := opts.NewRegistry(uri.Host, opts.Common)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if err = reg.Ping(context.Background()); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
+
+func TestRemote_NewRepository(t *testing.T) {
+	caPath := filepath.Join(t.TempDir(), "oras-test.pem")
+	if err := os.WriteFile(caPath, pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: ts.Certificate().Raw}), 0644); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	pool := x509.NewCertPool()
+	pool.AddCert(ts.Certificate())
+	opts := struct {
+		Remote
+		Common
+	}{
+		Remote{
+			CACertFilePath: caPath,
+		},
+		Common{},
+	}
+
+	uri, err := url.ParseRequestURI(ts.URL)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	repo, err := opts.NewRepository(uri.Host+"/"+testRepo, opts.Common)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if err = repo.Tags(context.Background(), func(got []string) error {
+		want := []string{"tag"}
+		if len(got) != len(testTagList.Tags) || !reflect.DeepEqual(got, want) {
+			return fmt.Errorf("expect: %v, got: %v", testTagList.Tags, got)
+		}
+		return nil
+	}); err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
 }
