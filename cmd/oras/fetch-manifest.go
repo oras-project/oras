3bed95850fe877e95f6d23e203baf25e185cdf1f
Billy Zha <jinzha1@microsoft.com>
2022-07-20 09:31:18 +0800

    Add platform and media type
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- cmd/oras/fetch-manifest.go
+++ cmd/oras/fetch-manifest.go
@@ -20,17 +20,20 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"os"
 	"strings"
 
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/spf13/cobra"
+	"oras.land/oras-go/v2"
+	"oras.land/oras-go/v2/content"
 	"oras.land/oras-go/v2/registry"
 	"oras.land/oras/cmd/oras/internal/option"
 )
 
-type getManifestOptions struct {
+type fetchManifestOptions struct {
 	option.Common
 	option.Remote
 
@@ -42,7 +45,7 @@ type getManifestOptions struct {
 }
 
 func fetchManifestCmd() *cobra.Command {
-	var opts getManifestOptions
+	var opts fetchManifestOptions
 	cmd := &cobra.Command{
 		Use:   "fetch-manifest [flags] <name:tag|name@digest>",
 		Short: "[Preview] Fetch manifest of the target artifact",
@@ -70,13 +73,13 @@ Example - Get manifest with raw json result:
 
 	cmd.Flags().BoolVarP(&opts.raw, "raw", "r", false, "output raw manifest without formatting")
 	cmd.Flags().IntVarP(&opts.indent, "indent", "n", 4, "number of spaces for indentation")
-	cmd.Flags().StringVarP(&opts.mediaType, "media-type", "", "", "number of spaces for indentation")
-	// cmd.Flags().StringVarP(&opts.platform, "platform", "p", "", "number of spaces for indentation")
+	cmd.Flags().StringVarP(&opts.mediaType, "media-type", "", "", "accepted media types")
+	cmd.Flags().StringVarP(&opts.platform, "platform", "p", "", "fetch manifest for a certain platform if target is multi-platform capable")
 	option.ApplyFlags(&opts, cmd.Flags())
 	return cmd
 }
 
-func getManifest(opts getManifestOptions) error {
+func getManifest(opts fetchManifestOptions) error {
 	ctx, _ := opts.SetLoggerLevel()
 	repo, err := opts.NewRepository(opts.targetRef, opts.Common)
 	if err != nil {
@@ -89,10 +92,19 @@ func getManifest(opts getManifestOptions) error {
 		repo.ManifestMediaTypes = []string{opts.mediaType}
 	}
 
-	// TODO: platform will be added when oras-project/oras-go#210 is done
-	_, manifest, err := fetchAndVerify(ctx, repo, opts.targetRef)
+	desc, manifest, err := fetchAndVerify(ctx, repo, opts.targetRef)
 	if err != nil {
 		return err
+	}
+	if opts.platform != "" {
+		// TODO: replace this with oras-go support when oras-project/oras-go#210 is done
+		if desc.MediaType != ocispec.MediaTypeImageIndex || desc.MediaType != "application/vnd.docker.distribution.manifest.list.v2+json" {
+			return fmt.Errorf("%q is not a multi-platform media type", desc.MediaType)
+		}
+		manifest, err = fetchPlatform(ctx, repo, manifest, opts)
+		if err != nil {
+			return err
+		}
 	}
 
 	// Output
@@ -106,6 +118,60 @@ func getManifest(opts getManifestOptions) error {
 	return nil
 }
 
+// TODO: replace this with oras-go support when oras-project/oras-go#210 is done
+func fetchPlatform(ctx context.Context, refFetcher oras.Target, root []byte, opts fetchManifestOptions) ([]byte, error) {
+	target, err := parsePlatform(opts.platform)
+	if err != nil {
+		return nil, err
+	}
+
+	var index ocispec.Index
+	if err := json.Unmarshal(root, &index); err != nil {
+		return nil, err
+	}
+
+	for _, m := range index.Manifests {
+		if target.OS == m.Platform.OS &&
+			target.Architecture == m.Platform.Architecture &&
+			(target.Variant == "" || target.Variant == m.Platform.Variant) &&
+			(target.OSVersion == "" || target.OSVersion == m.Platform.OSVersion) {
+			return content.FetchAll(ctx, refFetcher, m)
+		}
+	}
+	return nil, fmt.Errorf("failed to find platform matching the flag %q", opts.platform)
+}
+
+func parsePlatform(platform string) (ocispec.Platform, error) {
+	// Move me to a package with testing
+	if platform == "all" {
+		return ocispec.Platform{}, nil
+	}
+
+	var p ocispec.Platform
+
+	parts := strings.SplitN(platform, ":", 2)
+	if len(parts) == 2 {
+		p.OSVersion = parts[1]
+	}
+
+	parts = strings.Split(parts[0], "/")
+
+	if len(parts) < 2 {
+		return ocispec.Platform{}, fmt.Errorf("failed to parse platform '%s': expected format os/arch[/variant]", platform)
+	}
+	if len(parts) > 3 {
+		return ocispec.Platform{}, fmt.Errorf("failed to parse platform '%s': too many slashes", platform)
+	}
+
+	p.OS = parts[0]
+	p.Architecture = parts[1]
+	if len(parts) > 2 {
+		p.Variant = parts[2]
+	}
+
+	return p, nil
+}
+
 func fetchAndVerify(ctx context.Context, refFetcher registry.ReferenceFetcher, reference string) (ocispec.Descriptor, []byte, error) {
 	// TODO: replace this when oras-project/oras-go#102 is done
 	// Read and verify digest
@@ -114,7 +180,6 @@ func fetchAndVerify(ctx context.Context, refFetcher registry.ReferenceFetcher, r
 		return ocispec.Descriptor{}, nil, err
 	}
 	defer rc.Close()
-	// TODO: use io.ReadAll with validation when oras-project/oras-go#128 is done
 	verifier := desc.Digest.Verifier()
 	r := io.TeeReader(rc, verifier)
 	manifest := make([]byte, desc.Size)
