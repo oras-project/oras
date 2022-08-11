fff4426bef052265c04ed200a80205b051a3931c
Billy Zha <jinzha1@microsoft.com>
2022-08-11 16:27:15 +0800

    Add E2E tests
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 	github.com/docker/cli v20.10.17+incompatible
 	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6
 	github.com/need-being/go-tree v0.1.0
+	github.com/onsi/ginkgo/v2 v2.1.4
+	github.com/onsi/gomega v1.20.0
 	github.com/opencontainers/go-digest v1.0.0
 	github.com/opencontainers/image-spec v1.0.3-0.20211202183452-c5a74bcca799
 	github.com/oras-project/artifacts-spec v1.0.0-rc.2
@@ -21,9 +23,13 @@ require (
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/docker/docker v20.10.17+incompatible // indirect
 	github.com/docker/docker-credential-helpers v0.6.4 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/opencontainers/distribution-spec/specs-go v0.0.0-20220620172159-4ab4752c3b86 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ test/e2e/manifest_test.go
@@ -0,0 +1,125 @@
+/*
+Copyright The ORAS Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"io"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"oras.land/oras/cmd/oras/manifest"
+	"oras.land/oras/test/e2e/output"
+)
+
+const (
+	digest_latest       = "sha256:7d246653d0511db2a6b2e0436cfd0e52ac8c066000264b3ce63331ac66dca625"
+	manifest_latest     = `{"manifests":[{"digest":"sha256:f54a58bc1aac5ea1a25d796ae155dc228b3f0e11d046ae276b39c4bf2f13d8c4","mediaType":"application\/vnd.docker.distribution.manifest.v2+json","platform":{"architecture":"amd64","os":"linux"},"size":525},{"digest":"sha256:7b8b7289d0536a08eabdf71c20246e23f7116641db7e1d278592236ea4dcb30c","mediaType":"application\/vnd.docker.distribution.manifest.v2+json","platform":{"architecture":"arm","os":"linux","variant":"v5"},"size":525},{"digest":"sha256:f130bd2d67e6e9280ac6d0a6c83857bfaf70234e8ef4236876eccfbd30973b1c","mediaType":"application\/vnd.docker.distribution.manifest.v2+json","platform":{"architecture":"arm","os":"linux","variant":"v7"},"size":525},{"digest":"sha256:432f982638b3aefab73cc58ab28f5c16e96fdb504e8c134fc58dff4bae8bf338","mediaType":"application\/vnd.docker.distribution.manifest.v2+json","platform":{"architecture":"arm64","os":"linux","variant":"v8"},"size":525},{"digest":"sha256:995efde2e81b21d1ea7066aa77a59298a62a9e9fbb4b77f36c189774ec9b1089","mediaType":"application\/vnd.docker.distribution.manifest.v2+json","platform":{"architecture":"386","os":"linux"},"size":525},{"digest":"sha256:eb11b1a194ff8e236a01eff392c4e1296a53b0fb4780d8b0382f7996a15d5392","mediaType":"application\/vnd.docker.distribution.manifest.v2+json","platform":{"architecture":"mips64le","os":"linux"},"size":525},{"digest":"sha256:b836bb24a270b9cc935962d8228517fde0f16990e88893d935efcb1b14c0017a","mediaType":"application\/vnd.docker.distribution.manifest.v2+json","platform":{"architecture":"ppc64le","os":"linux"},"size":525},{"digest":"sha256:98c9722322be649df94780d3fbe594fce7996234b259f27eac9428b84050c849","mediaType":"application\/vnd.docker.distribution.manifest.v2+json","platform":{"architecture":"riscv64","os":"linux"},"size":525},{"digest":"sha256:c7b6944911848ce39b44ed660d95fb54d69bbd531de724c7ce6fc9f743c0b861","mediaType":"application\/vnd.docker.distribution.manifest.v2+json","platform":{"architecture":"s390x","os":"linux"},"size":525},{"digest":"sha256:3624dfaed3b147d49409b0306a2faedfc8da7117b1b59d81714632cef2367e57","mediaType":"application\/vnd.docker.distribution.manifest.v2+json","platform":{"architecture":"amd64","os":"windows","os.version":"10.0.20348.887"},"size":1125},{"digest":"sha256:f220cf100ada1cad5d2c1ce8aa6765da9a261f4cb3911ba5a1bf039769fa117b","mediaType":"application\/vnd.docker.distribution.manifest.v2+json","platform":{"architecture":"amd64","os":"windows","os.version":"10.0.17763.3287"},"size":1125}],"mediaType":"application\/vnd.docker.distribution.manifest.list.v2+json","schemaVersion":2}`
+	digest_linuxAMD64   = "sha256:f54a58bc1aac5ea1a25d796ae155dc228b3f0e11d046ae276b39c4bf2f13d8c4"
+	manifest_linuxAMD64 = `{
+   "schemaVersion": 2,
+   "mediaType": "application/vnd.docker.distribution.manifest.v2+json",
+   "config": {
+      "mediaType": "application/vnd.docker.container.image.v1+json",
+      "size": 1469,
+      "digest": "sha256:feb5d9fea6a5e9606aa995e879d862b825965ba48de054caab5ef356dc6b3412"
+   },
+   "layers": [
+      {
+        "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
+        "size": 2479,
+        "digest": "sha256:2db29710123e3e53a794f2694094b9b4338aa9ee5c40b930cb8063a1be392c54"
+      }
+   ]
+}
+`
+)
+
+var _ = Describe("Manifest", func() {
+	Context("user", func() {
+		When("look for supported command and help", func() {
+			It("list available commands", func() {
+				cmd := manifest.Cmd()
+				cmd.SetArgs([]string{"-h"})
+				km := output.NewKeywordMatcher([]string{
+					"[Preview] Manifest",
+					"[Preview] Fetch",
+				})
+				cmd.SetOutput(km)
+				Expect(cmd.Execute()).Should(Succeed(), "Execution should succeed")
+				want, matched := km.Status()
+				Expect(matched).To(Equal(want))
+			})
+			It("list fetch-related commands", func() {
+				cmd := manifest.Cmd()
+				cmd.SetArgs([]string{"fetch", "-h"})
+				km := output.NewKeywordMatcher([]string{
+					"** This command is in preview and under development. **",
+					"[Preview] Fetch",
+				})
+				cmd.SetOutput(km)
+				Expect(cmd.Execute()).Should(Succeed(), "Execution should succeed")
+				want, matched := km.Status()
+				Expect(matched).To(Equal(want))
+			})
+			It("use get alias", func() {
+				cmd := manifest.Cmd()
+				cmd.SetArgs([]string{"get", "-h"})
+				cmd.SetOutput(io.Discard)
+				Expect(cmd.Execute()).Should(Succeed(), "Execution should succeed")
+			})
+		})
+
+		When("fetching manifest", func() {
+			It("no artifact reference provided", func() {
+				cmd := manifest.Cmd()
+				cmd.SetArgs([]string{"fetch", "-d"})
+				km := output.NewKeywordMatcher([]string{"Error:"})
+				cmd.SetOutput(km)
+				Expect(cmd.Execute()).ShouldNot(Succeed(), "Execution should fail")
+				want, matched := km.Status()
+				Expect(matched).To(Equal(want))
+			})
+
+			It("no artifact reference provided", func() {
+				cmd := manifest.Cmd()
+				cmd.SetArgs([]string{"fetch", "-d"})
+				km := output.NewKeywordMatcher([]string{"Error:"})
+				cmd.SetOutput(km)
+				Expect(cmd.Execute()).ShouldNot(Succeed(), "Execution should fail")
+				want, matched := km.Status()
+				Expect(matched).To(Equal(want))
+			})
+
+			It("fetch manifest list with tag", func() {
+				cmd := manifest.Cmd()
+				cmd.SetArgs([]string{"fetch", "docker.io/library/hello-world:latest"})
+				w := output.NewWriter()
+				cmd.SetOutput(w)
+				Expect(cmd.Execute()).Should(Succeed(), "Execution should succeed")
+				Expect(string(w.ReadAll())).To(Equal(manifest_latest), "Should read out manifest content as expected")
+			})
+
+			It("fetch manifest list with digest", func() {
+				cmd := manifest.Cmd()
+				cmd.SetArgs([]string{"fetch", "docker.io/library/hello-world@" + digest_latest})
+				w := output.NewWriter()
+				cmd.SetOutput(w)
+				Expect(cmd.Execute()).Should(Succeed(), "Execution should succeed")
+				Expect(string(w.ReadAll())).To(Equal(manifest_latest), "Should read out manifest content as expected")
+			})
+		})
+	})
+})
