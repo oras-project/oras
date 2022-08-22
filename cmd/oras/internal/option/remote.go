bdb1ced9b9ba3c12da606828e1a41c3135d61c92
Billy Zha <jinzha1@microsoft.com>
2022-08-22 12:58:58 +0800

    fix: enable proxy in auth (#514)
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- cmd/oras/internal/option/remote.go
+++ cmd/oras/internal/option/remote.go
@@ -24,6 +24,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/spf13/pflag"
 	"oras.land/oras-go/v2/registry/remote"
@@ -116,8 +117,19 @@ func (opts *Remote) authClient(debug bool) (client *auth.Client, err error) {
 	}
 	client = &auth.Client{
 		Client: &http.Client{
+			// default value are derived from http.DefaultTransport
 			Transport: &http.Transport{
-				TLSClientConfig: config,
+				Proxy: http.ProxyFromEnvironment,
+				DialContext: (&net.Dialer{
+					Timeout:   30 * time.Second,
+					KeepAlive: 30 * time.Second,
+				}).DialContext,
+				ForceAttemptHTTP2:     true,
+				MaxIdleConns:          100,
+				IdleConnTimeout:       90 * time.Second,
+				TLSHandshakeTimeout:   10 * time.Second,
+				ExpectContinueTimeout: 1 * time.Second,
+				TLSClientConfig:       config,
 			},
 		},
 		Cache: auth.NewCache(),
