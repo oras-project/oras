dce8f20d2f32c11393332a66ea716b30247e5a9b
Juncheng Zhu <junczhu@microsoft.com>
2022-08-22 23:04:20 +0800

    refactor: repositoryCmd()
    Signed-off-by: Juncheng Zhu <junczhu@microsoft.com>

--- cmd/oras/repository/list.go
+++ cmd/oras/repository/list.go
@@ -28,39 +28,37 @@ type repositoryOptions struct {
 	hostname string
 }
 
-// func init() {
-// 	rootCmd.AddCommand(versionCmd)
-// }
-
-func Cmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "repository [command]",
-		Short: "repository operations",
-	}
-
-	cmd.AddCommand(repositoryCmd())
-	return cmd
-}
-
-func repositoryCmd() *cobra.Command {
+func listCmd() *cobra.Command {
 	var opts repositoryOptions
 	// in case need option
 	cmd := &cobra.Command{
 		Use:   "list [REGISTRY]",
-		Short: "Print the version number of Hugo",
-		Long:  `All software has versions. This is Hugo's`,
+		Short: "[Preview] List the repositories under the registry",
+		Long: `[Preview] List the repositories under the registry
+** This command is in preview and under development. **
+Example - Fetch raw manifest:
+  oras manifest fetch localhost:5000/hello:latest
+Example - Fetch the descriptor of a manifest:
+  oras manifest fetch --descriptor localhost:5000/hello:latest
+Example - Fetch manifest with specified media type:
+  oras manifest fetch --media-type 'application/vnd.oci.image.manifest.v1+json' localhost:5000/hello:latest
+Example - Fetch manifest with certain platform:
+  oras manifest fetch --platform 'linux/arm/v5' localhost:5000/hello:latest
+Example - Fetch manifest with prettified json result:
+  oras manifest fetch --pretty localhost:5000/hello:latest
+`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			opts.hostname = args[0]
-			return runRepository(opts)
+			return listRepository(opts)
 		},
 	}
-	// cmd.Flags()
 
+	// cmd.Flags()
 	option.ApplyFlags(&opts, cmd.Flags())
 	return cmd
 }
 
-func runRepository(opts repositoryOptions) error {
+func listRepository(opts repositoryOptions) error {
 	ctx, _ := opts.SetLoggerLevel()
 	// get all repository from the registry
 	reg, err := opts.Remote.NewRegistry(opts.hostname, opts.Common)
