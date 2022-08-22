4f09007a66d8bf9f7501fc728e94f34c23d77210
Billy Zha <jinzha1@microsoft.com>
2022-08-22 15:46:27 +0800

    bump: update version to release 0.14.0 (#515)
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// Version is the current version of the oras.
-	Version = "0.13.0"
+	Version = "0.14.0"
 	// BuildMetadata is the extra build time data
 	BuildMetadata = "unreleased"
 	// GitCommit is the git sha1
