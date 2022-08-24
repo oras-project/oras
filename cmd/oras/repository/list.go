6d7c238e903b897ebfbfa4a6f756a3d1bbe75fff
Juncheng Zhu <junczhu@microsoft.com>
2022-08-24 15:00:43 +0800

    feat: update repository list
    Signed-off-by: Juncheng Zhu <junczhu@microsoft.com>

--- cmd/oras/repository/list.go
+++ cmd/oras/repository/list.go
@@ -30,30 +30,21 @@ type repositoryOptions struct {
 
 func listCmd() *cobra.Command {
 	var opts repositoryOptions
-	// in case need option
 	cmd := &cobra.Command{
-		Use:   "list [REGISTRY]",
+		Use:   "list REGISTRY [flags]",
 		Short: "[Preview] List the repositories under the registry",
 		Long: `[Preview] List the repositories under the registry
 ** This command is in preview and under development. **
 Example - Fetch raw manifest:
-  oras manifest fetch localhost:5000/hello:latest
-Example - Fetch the descriptor of a manifest:
-  oras manifest fetch --descriptor localhost:5000/hello:latest
-Example - Fetch manifest with specified media type:
-  oras manifest fetch --media-type 'application/vnd.oci.image.manifest.v1+json' localhost:5000/hello:latest
-Example - Fetch manifest with certain platform:
-  oras manifest fetch --platform 'linux/arm/v5' localhost:5000/hello:latest
-Example - Fetch manifest with prettified json result:
-  oras manifest fetch --pretty localhost:5000/hello:latest
+  oras repository list localhost:5000
 `,
+		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			opts.hostname = args[0]
 			return listRepository(opts)
 		},
 	}
 
-	// cmd.Flags()
 	option.ApplyFlags(&opts, cmd.Flags())
 	return cmd
 }
