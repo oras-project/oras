452f04c31499b2df574b106c1bdfcb72f299131b
Billy Zha <jinzha1@microsoft.com>
2022-09-08 15:26:44 +0800

    fix read file
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- test/e2e/scenario/oras_artifact.go
+++ test/e2e/scenario/oras_artifact.go
@@ -14,7 +14,6 @@ limitations under the License.
 package scenario
 
 import (
-	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -75,7 +74,7 @@ var _ = Describe("ORAS user", Ordered, func() {
 				"push", utils.Reference(utils.Host, repo, tag), pathes[1], pathes[2], pathes[3], "--config", pathes[0], "-v", "--export-manifest", manifestPath)
 
 			ginkgo.It("should export the manifest", func() {
-				content, err := io.ReadFile(manifestPath)
+				content, err := os.ReadFile(manifestPath)
 				gomega.Expect(err).To(gomega.BeNil())
 				*s = string(content)
 			})
@@ -100,10 +99,10 @@ var _ = Describe("ORAS user", Ordered, func() {
 				"pull", utils.Reference(utils.Host, repo, tag), "-v", "--config", pathes[0], "-o", temp_path)
 			for i := range pathes {
 				ginkgo.It("should download file "+pathes[i], func() {
-					got, err := io.ReadFile(pathes[i])
+					got, err := os.ReadFile(pathes[i])
 					gomega.Expect(err).To(gomega.BeNil())
 
-					want, err := io.ReadFile(filepath.Join(temp_path+"-pushed", files[i]))
+					want, err := os.ReadFile(filepath.Join(temp_path+"-pushed", files[i]))
 					gomega.Expect(err).To(gomega.BeNil())
 					gomega.Expect(string(got)).To(gomega.Equal(string(want)))
 				})
