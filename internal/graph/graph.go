b50aa2385702b6c01b84d57d34bbcd3aae8c22e5
Billy Zha <jinzha1@microsoft.com>
2022-11-11 11:37:50 +0800

    remove named parameters
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- internal/graph/graph.go
+++ internal/graph/graph.go
@@ -27,17 +27,21 @@ import (
 // Successors returns the nodes directly pointed by the current node, picking
 // out subject and config descriptor if applicable.
 // Returning nil when no subject and config found.
-func Successors(ctx context.Context, fetcher content.Fetcher, node ocispec.Descriptor) (nodes []ocispec.Descriptor, subject *ocispec.Descriptor, config *ocispec.Descriptor, err error) {
+func Successors(ctx context.Context, fetcher content.Fetcher, node ocispec.Descriptor) ([]ocispec.Descriptor, *ocispec.Descriptor, *ocispec.Descriptor, error) {
 	var fetched []byte
+	var nodes []ocispec.Descriptor
+	var subject *ocispec.Descriptor
+	var config *ocispec.Descriptor
+	var err error
 	switch node.MediaType {
 	case docker.MediaTypeManifest, ocispec.MediaTypeImageManifest:
 		fetched, err = content.FetchAll(ctx, fetcher, node)
 		if err != nil {
-			return
+			return nil, nil, nil, err
 		}
 		var manifest ocispec.Manifest
 		if err = json.Unmarshal(fetched, &manifest); err != nil {
-			return
+			return nil, nil, nil, err
 		}
 		subject = manifest.Subject
 		config = &manifest.Config
@@ -45,16 +49,19 @@ func Successors(ctx context.Context, fetcher content.Fetcher, node ocispec.Descr
 	case ocispec.MediaTypeArtifactManifest:
 		fetched, err = content.FetchAll(ctx, fetcher, node)
 		if err != nil {
-			return
+			return nil, nil, nil, err
 		}
 		var manifest ocispec.Artifact
 		if err = json.Unmarshal(fetched, &manifest); err != nil {
-			return
+			return nil, nil, nil, err
 		}
 		subject = manifest.Subject
 		nodes = manifest.Blobs
 	default:
 		nodes, err = content.Successors(ctx, fetcher, node)
+		if err != nil {
+			return nil, nil, nil, err
+		}
 	}
-	return
+	return nodes, subject, config, nil
 }
