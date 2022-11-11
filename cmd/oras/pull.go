c80d9ddf9c4b111bd31c2d26d38bdb4511157327
Billy Zha <jinzha1@microsoft.com>
2022-11-11 16:21:35 +0800

    remove subject
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- cmd/oras/pull.go
+++ cmd/oras/pull.go
@@ -38,7 +38,6 @@ type pullOptions struct {
 	option.Remote
 	option.Platform
 
-	includeSubject    bool
 	concurrency       int64
 	targetRef         string
 	KeepOldFiles      bool
@@ -85,7 +84,6 @@ Example - Pull all files with concurrency level tuned:
 
 	cmd.Flags().BoolVarP(&opts.KeepOldFiles, "keep-old-files", "k", false, "do not replace existing files when pulling, treat them as errors")
 	cmd.Flags().BoolVarP(&opts.PathTraversal, "allow-path-traversal", "T", false, "allow storing files out of the output directory")
-	cmd.Flags().BoolVarP(&opts.includeSubject, "include-subject", "", false, "recursively pull subjects")
 	cmd.Flags().StringVarP(&opts.Output, "output", "o", ".", "output directory")
 	cmd.Flags().StringVarP(&opts.ManifestConfigRef, "config", "", "", "output manifest config file")
 	cmd.Flags().Int64VarP(&opts.concurrency, "concurrency", "", 3, "concurrency level")
@@ -144,12 +142,9 @@ func runPull(opts pullOptions) error {
 			return rc, nil
 		})
 
-		nodes, subject, config, err := graph.Successors(ctx, statusFetcher, desc)
+		nodes, _, config, err := graph.Successors(ctx, statusFetcher, desc)
 		if err != nil {
 			return nil, err
-		}
-		if opts.includeSubject && subject != nil {
-			nodes = append(nodes, *subject)
 		}
 		if config != nil {
 			if targetConfig == nil {
