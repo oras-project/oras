88ad4862675dbbd17b07b99a6a1631e2db61b87c
Billy Zha <jinzha1@microsoft.com>
2022-11-14 18:56:45 +0800

    correct comments
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- cmd/oras/pull.go
+++ cmd/oras/pull.go
@@ -152,9 +152,9 @@ func runPull(opts pullOptions) error {
 		if config != nil {
 			getConfigOnce.Do(func() {
 				if configPath != "" && (configMediaType == "" || config.MediaType == configMediaType) {
-					// Save the target config when config provided and:
-					// 1) MediaType matches, or
-					// 2) MediaType not specified but current node is config
+					// Save the target config when
+					// 1) Config path provided, and
+					// 2) MediaType matches, or
 					if config.Annotations == nil {
 						config.Annotations = make(map[string]string)
 					}
