b2731a0d89a6e6256406a08e39bdf287fdae488e
Billy Zha <jinzha1@microsoft.com>
2022-11-21 18:53:56 +0800

    fix windows test
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- cmd/oras/internal/argument/file_windows.go
+++ cmd/oras/internal/argument/file_windows.go
@@ -23,18 +23,14 @@ import (
 // ParseFileReference parses file reference on windows.
 // Windows systems does not allow ':' in the file path except for drive letter.
 func ParseFileReference(reference string, mediaType string) (filePath, mediatype string) {
-	i := strings.Index(reference, ":")
+	i := strings.LastIndex(reference, ":")
 	if i < 0 {
 		return reference, mediaType
 	}
 
 	// In case it is C:\
 	if i == 1 && len(reference) > 2 && reference[2] == '\\' && unicode.IsLetter(rune(reference[0])) {
-		i = strings.Index(reference[3:], ":")
-		if i < 0 {
-			return reference, mediaType
-		}
-		i += 3
+		return reference, mediaType
 	}
 	return reference[:i], reference[i+1:]
 }
