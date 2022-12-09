68cdd9a41315428f6a7890df479dd6683608b27f
Billy Zha <jinzha1@microsoft.com>
2022-12-09 12:09:09 +0800

    add unauth
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- test/e2e/suite/command/tag.go
+++ test/e2e/suite/command/tag.go
@@ -22,11 +22,11 @@ import (
 
 var repoFmt = fmt.Sprintf("command/tag/%%s/%d/%%s", GinkgoRandomSeed())
 
-var _ = Describe("ORAS beginners:", Focus, func() {
+var _ = Describe("ORAS beginners:", func() {
 	When("running repo command", func() {
 		runAndShowPreviewInHelp([]string{"tag"})
 		It("should fail when no manifest reference provided", func() {
-			ORAS("tag").MatchKeyWords("[Preview] List", preview_desc, example_desc).Exec()
+			ORAS("tag").WithFailureCheck().MatchErrKeyWords("Error:").Exec()
 		})
 
 		It("should fail when provided manifest reference is not found", func() {
@@ -35,23 +35,23 @@ var _ = Describe("ORAS beginners:", Focus, func() {
 	})
 })
 
-var _ = Describe("Common registry users:", Focus, func() {
-	var tagAndValidate = func(ref string, tags ...string) {
-		ORAS(append([]string{"tag", ref}, tags...)...).MatchKeyWords(repo).Exec()
-		ORAS("repo", "tags").MatchKeyWords(tags...).Exec()
+var _ = Describe("Common registry users:", func() {
+	var tagAndValidate = func(reg string, repo string, tagOrDigest string, tags ...string) {
+		ORAS(append([]string{"tag", Reference(reg, repo, tagOrDigest)}, tags...)...).MatchKeyWords(tags...).Exec()
+		ORAS("repo", "tags", Reference(reg, repo, "")).MatchKeyWords(tags...).Exec()
 	}
 	When("running `tag`", func() {
 		It("should add a tag to an existent manifest when providing tag reference", func() {
-			tagAndValidate(Reference(Host, repo, multiImage), "tag-via-tag")
+			tagAndValidate(Host, repo, multiImage, "tag-via-tag")
 		})
 		It("should add a tag to an existent manifest when providing digest reference", func() {
-			tagAndValidate(Reference(Host, repo, digest_multi), "tag-via-digest")
+			tagAndValidate(Host, repo, digest_multi, "tag-via-digest")
 		})
 		It("should add multiple tags to an existent manifest when providing digest reference", func() {
-			tagAndValidate(Reference(Host, repo, digest_multi), "tag1-via-digest", "tag2-via-digest", "tag3-via-digest")
+			tagAndValidate(Host, repo, digest_multi, "tag1-via-digest", "tag2-via-digest", "tag3-via-digest")
 		})
 		It("should add multiple tags to an existent manifest when providing tag reference", func() {
-			tagAndValidate(Reference(Host, repo, multiImage), "tag1-via-tag", "tag1-via-tag", "tag1-via-tag")
+			tagAndValidate(Host, repo, multiImage, "tag1-via-tag", "tag1-via-tag", "tag1-via-tag")
 		})
 	})
 })
