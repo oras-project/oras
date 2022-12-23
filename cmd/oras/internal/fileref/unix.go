219e3cc782cf7d5e99cff2781aa8367e22945e71
Billy Zha <jinzha1@microsoft.com>
2022-12-23 17:02:52 +0800

    fix test
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- cmd/oras/internal/fileref/unix.go
+++ cmd/oras/internal/fileref/unix.go
@@ -23,13 +23,16 @@ import (
 )
 
 // Parse parses file reference on unix.
-func Parse(reference string, mediaType string) (filePath, mediatype string, err error) {
+func Parse(reference string, defaultMediaType string) (filePath, mediaType string, err error) {
+	filePath = reference
+	mediaType = defaultMediaType
+
 	i := strings.LastIndex(reference, ":")
 	if i >= 0 {
-		filePath, mediatype = reference[:i], reference[i+1:]
+		filePath, mediaType = reference[:i], reference[i+1:]
 	}
 	if filePath == "" {
 		return "", "", fmt.Errorf("found empty file path in %q", reference)
 	}
-	return filePath, mediatype, nil
+	return filePath, mediaType, nil
 }
