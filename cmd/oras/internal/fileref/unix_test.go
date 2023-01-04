e8a3b290ce5dce54ff3b808d2c15da1dc5b70f20
Billy Zha <jinzha1@microsoft.com>
2023-01-04 15:38:48 +0800

    fix unit test
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- cmd/oras/internal/fileref/unix_test.go
+++ cmd/oras/internal/fileref/unix_test.go
@@ -36,7 +36,7 @@ func Test_Parse_fileReference(t *testing.T) {
 		{"file name and media type, default type ignored", args{"az:b", "c"}, "az", "b"},
 		{"file name and empty media type, default type ignored", args{"az:", "c"}, "az", ""},
 		{"colon file name and media type", args{`az\:b:c`, "d"}, "az:b", "c"},
-		{"colon file name with backslash and media type with backslash", args{`az\\\:b:c\\d`, "d"}, `az\:b`, "c\d"},
+		{"colon file name with backslash and media type with backslash", args{`az\\\:b:c\\d`, "d"}, `az\:b`, `c\d`},
 		{"colon file name and empty media type", args{"az:b:", "c"}, "az:b", ""},
 		{"colon-prefix file name and media type", args{":az:b:c", "d"}, ":az:b", "c"},
 
