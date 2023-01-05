e1611361fb1ebecd390f66d52aec9780ca1c2a25
Billy Zha <jinzha1@microsoft.com>
2023-01-05 18:44:56 +0800

    remove demo version
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -17,7 +17,7 @@ package version
 
 var (
 	// Version is the current version of the oras.
-	Version = "0.16.0-oci-layout-demo"
+	Version = "0.16.0"
 	// BuildMetadata is the extra build time data
 	BuildMetadata = "unreleased"
 	// GitCommit is the git sha1
