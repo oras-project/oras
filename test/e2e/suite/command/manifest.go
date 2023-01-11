6170e4d9a49315664c30eb34728c051391e95aee
Billy Zha <jinzha1@microsoft.com>
2023-01-11 19:55:38 +0800

    fix manifest e2e
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- test/e2e/suite/command/manifest.go
+++ test/e2e/suite/command/manifest.go
@@ -106,8 +106,8 @@ var _ = Describe("ORAS beginners:", func() {
 			})
 
 			It("should fail to delete a non-existent manifest and output descriptor via tag, without force flag set", func() {
-				toDeleteRef := Reference(Host, Repo, "this.tag.should-not.be-existed", "--force", "--descriptor")
-				ORAS("manifest", "delete", toDeleteRef).
+				toDeleteRef := Reference(Host, Repo, "this.tag.should-not.be-existed")
+				ORAS("manifest", "delete", toDeleteRef, "--force", "--descriptor").
 					ExpectFailure().
 					MatchErrKeyWords(toDeleteRef, "the specified manifest does not exist").
 					Exec()
