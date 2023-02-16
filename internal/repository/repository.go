89dc7a53a46d3d1cdada5cd00d0b5651cf3596fd
Billy Zha <jinzha1@microsoft.com>
2023-02-16 23:53:01 +0800

    chore: move ParseRepoPath to internal package (#817)
    Putting it in `cmd/oras/internal` will trigger code coverage scan on
    command declaration code, which effects our code coverage.
    
    ---------
    
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

---
+++ internal/repository/repository.go
@@ -0,0 +1,43 @@
+/*
+Copyright The ORAS Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package repository
+
+import (
+	"fmt"
+	"strings"
+
+	"oras.land/oras-go/v2/registry"
+)
+
+// ParserRepoPath extracts hostname and namespace from rawReference.
+func ParseRepoPath(rawReference string) (hostname, namespace string, err error) {
+	rawReference = strings.TrimSuffix(rawReference, "/")
+	if strings.Contains(rawReference, "/") {
+		var ref registry.Reference
+		ref, err = registry.ParseReference(rawReference)
+		if err != nil {
+			return
+		}
+		if ref.Reference != "" {
+			err = fmt.Errorf("tags or digests should not be provided")
+			return
+		}
+		hostname = ref.Registry
+		namespace = ref.Repository + "/"
+	} else {
+		hostname = rawReference
+	}
+	return
+}
