a88d6d2ff552cbec178d4bb31ad24fafc50c34a9
Billy Zha <jinzha1@microsoft.com>
2023-03-06 08:36:27 +0000

    fix e2e
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- test/e2e/suite/command/resolve_host.go
+++ test/e2e/suite/command/resolve_host.go
@@ -82,40 +82,40 @@ var _ = Describe("Common registry users:", func() {
 
 			It("should attach a file to a subject via resolve flag", func() {
 				repo := testRepo("attach")
-				tempDir := CopyTestDataToTemp()
-				prepare(Reference(Host, ImageRepo, foobar.Tag), Reference(Host, repo, foobar.Tag))
+				tempDir := PrepareTempFiles(()
+				prepare(RegistryRef(Host, ImageRepo, foobar.Tag), RegistryRef(Host, repo, foobar.Tag))
 
-				ORAS(append([]string{"attach", "--artifact-type", "test.attach", Reference(mockedHost, repo, foobar.Tag), fmt.Sprintf("%s:%s", foobar.AttachFileName, foobar.AttachFileMedia)}, unary...)...).
+				ORAS(append([]string{"attach", "--artifact-type", "test.attach", RegistryRef(mockedHost, repo, foobar.Tag), fmt.Sprintf("%s:%s", foobar.AttachFileName, foobar.AttachFileMedia)}, unary...)...).
 					WithWorkDir(tempDir).
 					MatchStatus([]match.StateKey{foobar.AttachFileStateKey}, false, 1).Exec()
 				// validate
 				var index ocispec.Index
-				bytes := ORAS("discover", "-o", "json", Reference(Host, repo, foobar.Tag)).Exec().Out.Contents()
+				bytes := ORAS("discover", "-o", "json", RegistryRef(Host, repo, foobar.Tag)).Exec().Out.Contents()
 				Expect(json.Unmarshal(bytes, &index)).ShouldNot(HaveOccurred())
 				Expect(len(index.Manifests)).To(Equal(1))
 			})
 			It("should push a blob from a stdin and output the descriptor with specific media-type", func() {
 				mediaType := "test.media"
 				repo := testRepo("blob/push")
-				ORAS(append([]string{"blob", "push", Reference(mockedHost, repo, pushDigest), "-", "--media-type", mediaType, "--descriptor", "--size", strconv.Itoa(len(pushContent))}, unary...)...).
+				ORAS(append([]string{"blob", "push", RegistryRef(mockedHost, repo, pushDigest), "-", "--media-type", mediaType, "--descriptor", "--size", strconv.Itoa(len(pushContent))}, unary...)...).
 					WithInput(strings.NewReader(pushContent)).
 					MatchContent(fmt.Sprintf(pushDescFmt, mediaType)).Exec()
-				ORAS("blob", "fetch", Reference(Host, repo, pushDigest), "--output", "-").MatchContent(pushContent).Exec()
+				ORAS("blob", "fetch", RegistryRef(Host, repo, pushDigest), "--output", "-").MatchContent(pushContent).Exec()
 			})
 			It("should fetch blob descriptor ", func() {
-				ORAS(append([]string{"blob", "fetch", Reference(mockedHost, ImageRepo, foobar.FooBlobDigest), "--descriptor"}, unary...)...).
+				ORAS(append([]string{"blob", "fetch", RegistryRef(mockedHost, ImageRepo, foobar.FooBlobDigest), "--descriptor"}, unary...)...).
 					MatchContent(foobar.FooBlobDescriptor).Exec()
 			})
 
 			It("should delete a blob with force flag and output descriptor", func() {
 				repo := testRepo("blob/delete")
-				ORAS("cp", Reference(Host, ImageRepo, foobar.Digest), Reference(Host, repo, foobar.Digest)).Exec()
+				ORAS("cp", RegistryRef(Host, ImageRepo, foobar.Digest), RegistryRef(Host, repo, foobar.Digest)).Exec()
 				// test
-				toDeleteRef := Reference(mockedHost, repo, foobar.FooBlobDigest)
+				toDeleteRef := RegistryRef(mockedHost, repo, foobar.FooBlobDigest)
 				ORAS(append([]string{"blob", "delete", toDeleteRef, "--force", "--descriptor"}, unary...)...).
 					MatchContent(foobar.FooBlobDescriptor).
 					Exec()
-				deletedRef := Reference(Host, repo, foobar.FooBlobDigest)
+				deletedRef := RegistryRef(Host, repo, foobar.FooBlobDigest)
 				ORAS("blob", "delete", deletedRef).
 					WithDescription("validate").
 					ExpectFailure().
@@ -125,46 +125,46 @@ var _ = Describe("Common registry users:", func() {
 
 			It("should copy an image with source resolve DNS rules", func() {
 				repo := testRepo("cp/from")
-				dst := Reference(Host, repo, "copied")
-				ORAS(append([]string{"cp", Reference(mockedHost, ImageRepo, foobar.Tag), dst, "-v"}, from...)...).
+				dst := RegistryRef(Host, repo, "copied")
+				ORAS(append([]string{"cp", RegistryRef(mockedHost, ImageRepo, foobar.Tag), dst, "-v"}, from...)...).
 					MatchStatus(foobarStates, true, len(foobarStates)).
 					Exec()
-				CompareRef(Reference(Host, ImageRepo, foobar.Tag), dst)
+				CompareRef(RegistryRef(Host, ImageRepo, foobar.Tag), dst)
 			})
 
 			It("should copy an image with destination resolve DNS rules", func() {
-				src := Reference(Host, ImageRepo, foobar.Tag)
+				src := RegistryRef(Host, ImageRepo, foobar.Tag)
 				repo := testRepo("cp/to")
 				tag := "copied"
-				ORAS(append([]string{"cp", src, Reference(mockedHost, repo, tag), "-v"}, to...)...).
+				ORAS(append([]string{"cp", src, RegistryRef(mockedHost, repo, tag), "-v"}, to...)...).
 					MatchStatus(foobarStates, true, len(foobarStates)).
 					Exec()
-				CompareRef(src, Reference(Host, repo, tag))
+				CompareRef(src, RegistryRef(Host, repo, tag))
 			})
 
 			It("should copy an image with base resolve DNS rules", func() {
-				src := Reference(Host, ImageRepo, foobar.Tag)
+				src := RegistryRef(Host, ImageRepo, foobar.Tag)
 				repo := testRepo("cp/base")
 				tag := "copied"
 				flags := append(to[2:], "--resolve", unary[1]) //hack to chop the destination flags off
-				ORAS(append([]string{"cp", src, Reference(mockedHost, repo, tag), "-v"}, flags...)...).
+				ORAS(append([]string{"cp", src, RegistryRef(mockedHost, repo, tag), "-v"}, flags...)...).
 					MatchStatus(foobarStates, true, len(foobarStates)).
 					Exec()
-				CompareRef(src, Reference(Host, repo, tag))
+				CompareRef(src, RegistryRef(Host, repo, tag))
 			})
 
 			It("should copy an image with overridden resolve DNS rules for both source and destination registry", func() {
 				repo := testRepo("cp/override")
 				tag := "copied"
 				flags := append(append(to, from...), "--resolve", mockedHost+":80:1.1.1.1:5000")
-				ORAS(append([]string{"cp", Reference(mockedHost, ImageRepo, foobar.Tag), Reference(mockedHost, repo, tag), "-v"}, flags...)...).
+				ORAS(append([]string{"cp", RegistryRef(mockedHost, ImageRepo, foobar.Tag), RegistryRef(mockedHost, repo, tag), "-v"}, flags...)...).
 					MatchStatus(foobarStates, true, len(foobarStates)).
 					Exec()
-				CompareRef(Reference(Host, ImageRepo, foobar.Tag), Reference(Host, repo, tag))
+				CompareRef(RegistryRef(Host, ImageRepo, foobar.Tag), RegistryRef(Host, repo, tag))
 			})
 
 			It("should discover direct referrers of a subject", func() {
-				bytes := ORAS(append([]string{"discover", Reference(mockedHost, ArtifactRepo, foobar.Tag), "-o", "json"}, unary...)...).Exec().Out.Contents()
+				bytes := ORAS(append([]string{"discover", RegistryRef(mockedHost, ArtifactRepo, foobar.Tag), "-o", "json"}, unary...)...).Exec().Out.Contents()
 				var index ocispec.Index
 				Expect(json.Unmarshal(bytes, &index)).ShouldNot(HaveOccurred())
 				Expect(index.Manifests).To(HaveLen(2))
@@ -177,23 +177,23 @@ var _ = Describe("Common registry users:", func() {
 			})
 
 			It("should fetch index manifest with digest", func() {
-				ORAS(append([]string{"manifest", "fetch", Reference(Host, ImageRepo, multi_arch.Tag)}, unary...)...).
+				ORAS(append([]string{"manifest", "fetch", RegistryRef(Host, ImageRepo, multi_arch.Tag)}, unary...)...).
 					MatchContent(multi_arch.Manifest).Exec()
 			})
 
 			It("should fetch scratch config", func() {
-				ORAS(append([]string{"manifest", "fetch-config", Reference(Host, ImageRepo, foobar.Tag)}, unary...)...).
+				ORAS(append([]string{"manifest", "fetch-config", RegistryRef(Host, ImageRepo, foobar.Tag)}, unary...)...).
 					MatchContent("{}").Exec()
 			})
 
 			It("should do forced deletion and output descriptor", func() {
 				repo := testRepo("manifest/delete")
 				tempTag := "to-delete"
-				prepare(Reference(Host, ImageRepo, foobar.Tag), Reference(Host, repo, tempTag))
-				ORAS(append([]string{"manifest", "delete", Reference(mockedHost, repo, tempTag), "-f", "--descriptor"}, unary...)...).
+				prepare(RegistryRef(Host, ImageRepo, foobar.Tag), RegistryRef(Host, repo, tempTag))
+				ORAS(append([]string{"manifest", "delete", RegistryRef(mockedHost, repo, tempTag), "-f", "--descriptor"}, unary...)...).
 					MatchContent("{\"mediaType\":\"application/vnd.oci.image.manifest.v1+json\",\"digest\":\"sha256:fd6ed2f36b5465244d5dc86cb4e7df0ab8a9d24adc57825099f522fe009a22bb\",\"size\":851}").
 					WithDescription("cancel without confirmation").Exec()
-				validateTag(Reference(Host, repo, ""), tempTag, true)
+				validateTag(RegistryRef(Host, repo, ""), tempTag, true)
 			})
 
 			manifest := `{"schemaVersion":2,"mediaType":"application/vnd.oci.image.manifest.v1+json","config":{"mediaType":"application/vnd.oci.image.config.v1+json","digest":"sha256:44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a","size":53},"layers":[]}`
@@ -201,21 +201,21 @@ var _ = Describe("Common registry users:", func() {
 			It("should push a manifest from stdin without media type flag", func() {
 				repo := testRepo("manifest/push")
 				tag := "from-stdin"
-				prepare(Reference(Host, ImageRepo, foobar.Digest), Reference(Host, repo, foobar.Digest))
+				prepare(RegistryRef(Host, ImageRepo, foobar.Digest), RegistryRef(Host, repo, foobar.Digest))
 				//test
-				ORAS(append([]string{"manifest", "push", Reference(Host, repo, tag), "-"}, unary...)...).
-					MatchKeyWords("Pushed", Reference(Host, repo, tag), "Digest:", digest).
+				ORAS(append([]string{"manifest", "push", RegistryRef(Host, repo, tag), "-"}, unary...)...).
+					MatchKeyWords("Pushed", RegistryRef(Host, repo, tag), "Digest:", digest).
 					WithInput(strings.NewReader(manifest)).Exec()
-				validateTag(Reference(Host, repo, ""), tag, false)
+				validateTag(RegistryRef(Host, repo, ""), tag, false)
 			})
 
 			It("should pull all files in an image", func() {
 				pullRoot := "pulled"
 				configName := "test.config"
-				tempDir := CopyTestDataToTemp()
+				tempDir := PrepareTempFiles()
 				stateKeys := append(foobar.ImageLayerStateKeys, foobar.ManifestStateKey, foobar.ImageConfigStateKey(configName))
 				// test
-				ORAS(append([]string{"pull", Reference(mockedHost, ImageRepo, foobar.Tag), "-v", "--config", configName, "-o", pullRoot}, unary...)...).
+				ORAS(append([]string{"pull", RegistryRef(mockedHost, ImageRepo, foobar.Tag), "-v", "--config", configName, "-o", pullRoot}, unary...)...).
 					MatchStatus(stateKeys, true, len(stateKeys)).
 					WithWorkDir(tempDir).Exec()
 				// validate config and layers
@@ -234,8 +234,8 @@ var _ = Describe("Common registry users:", func() {
 
 			It("should push files without customized media types", func() {
 				repo := testRepo("push")
-				tempDir := CopyTestDataToTemp()
-				ORAS(append([]string{"push", Reference(mockedHost, repo, foobar.Tag), "-v"}, append(foobar.FileLayerNames, unary...)...)...).
+				tempDir := PrepareTempFiles(()
+				ORAS(append([]string{"push", RegistryRef(mockedHost, repo, foobar.Tag), "-v"}, append(foobar.FileLayerNames, unary...)...)...).
 					MatchStatus(foobar.FileStateKeys, true, len(foobar.FileStateKeys)).
 					WithWorkDir(tempDir).Exec()
 			})
@@ -245,15 +245,15 @@ var _ = Describe("Common registry users:", func() {
 			})
 
 			It("should list tags", func() {
-				ORAS(append([]string{"repository", "show-tags", Reference(mockedHost, ImageRepo, foobar.Digest)}, unary...)...).MatchKeyWords(foobar.Tag).Exec()
+				ORAS(append([]string{"repository", "show-tags", RegistryRef(mockedHost, ImageRepo, foobar.Digest)}, unary...)...).MatchKeyWords(foobar.Tag).Exec()
 			})
 
 			It("should add a tag to an existent manifest when providing tag reference", func() {
 				repo := testRepo("tag")
-				prepare(Reference(Host, ImageRepo, foobar.Digest), Reference(Host, repo, foobar.Digest))
+				prepare(RegistryRef(Host, ImageRepo, foobar.Digest), RegistryRef(Host, repo, foobar.Digest))
 				newTag := "tag-via-digest"
-				ORAS(append([]string{"tag", Reference(mockedHost, repo, foobar.Digest), newTag}, unary...)...).MatchKeyWords(newTag).Exec()
-				ORAS("repo", "tags", Reference(Host, repo, "")).MatchKeyWords(newTag).Exec()
+				ORAS(append([]string{"tag", RegistryRef(mockedHost, repo, foobar.Digest), newTag}, unary...)...).MatchKeyWords(newTag).Exec()
+				ORAS("repo", "tags", RegistryRef(Host, repo, "")).MatchKeyWords(newTag).Exec()
 			})
 		})
 	}
