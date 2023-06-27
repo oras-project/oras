e0d769f0a2347ff52289e9cb0fa374fbb058f22c
suganyas <ssuganyatce@gmail.com>
2023-06-27 22:57:41 +1000

    Fix the build for compilation error
    Signed-off-by: suganyas <ssuganyatce@gmail.com>

--- cmd/oras/internal/option/packer.go
+++ cmd/oras/internal/option/packer.go
@@ -77,7 +77,7 @@ func (opts *Packer) Parse() error {
 	if !opts.PathValidationDisabled {
 		for _, path := range opts.FileRefs {
 			// Remove the type if specified in the path <file>[:<type>] format
-			path, _, err = fileref.Parse(path, "")
+			path, _, err := fileref.Parse(path, "")
 			if err != nil {
 				return err
 			}
