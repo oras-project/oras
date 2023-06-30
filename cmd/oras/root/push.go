33cacb408c5f924698a6a7e5ceda36d253146228
Billy Zha <jinzha1@microsoft.com>
2023-06-30 03:31:36 +0000

    use memory as cache
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- cmd/oras/root/push.go
+++ cmd/oras/root/push.go
@@ -27,9 +27,11 @@ import (
 	"oras.land/oras-go/v2"
 	"oras.land/oras-go/v2/content"
 	"oras.land/oras-go/v2/content/file"
+	"oras.land/oras-go/v2/content/memory"
 	"oras.land/oras/cmd/oras/internal/display"
 	"oras.land/oras/cmd/oras/internal/fileref"
 	"oras.land/oras/cmd/oras/internal/option"
+	"oras.land/oras/internal/cache"
 )
 
 type pushOptions struct {
@@ -139,6 +141,8 @@ func runPush(ctx context.Context, opts pushOptions) error {
 		return err
 	}
 	defer store.Close()
+	memStore := memory.New()
+	proxy := cache.New(store, memStore)
 	if opts.manifestConfigRef != "" {
 		path, cfgMediaType, err := fileref.Parse(opts.manifestConfigRef, oras.MediaTypeUnknownConfig)
 		if err != nil {
@@ -160,11 +164,11 @@ func runPush(ctx context.Context, opts pushOptions) error {
 		return err
 	}
 	pack := func() (ocispec.Descriptor, error) {
-		root, err := oras.Pack(ctx, store, opts.artifactType, descs, packOpts)
+		root, err := oras.Pack(ctx, memStore, opts.artifactType, descs, packOpts)
 		if err != nil {
 			return ocispec.Descriptor{}, err
 		}
-		if err = store.Tag(ctx, root, root.Digest.String()); err != nil {
+		if err = memStore.Tag(ctx, root, root.Digest.String()); err != nil {
 			return ocispec.Descriptor{}, err
 		}
 		return root, nil
@@ -177,12 +181,12 @@ func runPush(ctx context.Context, opts pushOptions) error {
 	}
 	copyOptions := oras.DefaultCopyOptions
 	copyOptions.Concurrency = opts.concurrency
-	updateDisplayOption(&copyOptions.CopyGraphOptions, store, opts.Verbose)
+	updateDisplayOption(&copyOptions.CopyGraphOptions, proxy, opts.Verbose)
 	copy := func(root ocispec.Descriptor) error {
 		if tag := opts.Reference; tag == "" {
-			err = oras.CopyGraph(ctx, store, dst, root, copyOptions.CopyGraphOptions)
+			err = oras.CopyGraph(ctx, proxy, dst, root, copyOptions.CopyGraphOptions)
 		} else {
-			_, err = oras.Copy(ctx, store, root.Digest.String(), dst, tag, copyOptions)
+			_, err = oras.Copy(ctx, proxy, root.Digest.String(), dst, tag, copyOptions)
 		}
 		return err
 	}
@@ -195,7 +199,7 @@ func runPush(ctx context.Context, opts pushOptions) error {
 	fmt.Println("Pushed", opts.AnnotatedReference())
 
 	if len(opts.extraRefs) != 0 {
-		contentBytes, err := content.FetchAll(ctx, store, root)
+		contentBytes, err := content.FetchAll(ctx, proxy, root)
 		if err != nil {
 			return err
 		}
@@ -209,10 +213,10 @@ func runPush(ctx context.Context, opts pushOptions) error {
 	fmt.Println("Digest:", root.Digest)
 
 	// Export manifest
-	return opts.ExportManifest(ctx, store, root)
+	return opts.ExportManifest(ctx, proxy, root)
 }
 
-func updateDisplayOption(opts *oras.CopyGraphOptions, store content.Fetcher, verbose bool) {
+func updateDisplayOption(opts *oras.CopyGraphOptions, fetcher content.Fetcher, verbose bool) {
 	committed := &sync.Map{}
 	opts.PreCopy = display.StatusPrinter("Uploading", verbose)
 	opts.OnCopySkipped = func(ctx context.Context, desc ocispec.Descriptor) error {
@@ -221,7 +225,7 @@ func updateDisplayOption(opts *oras.CopyGraphOptions, store content.Fetcher, ver
 	}
 	opts.PostCopy = func(ctx context.Context, desc ocispec.Descriptor) error {
 		committed.Store(desc.Digest.String(), desc.Annotations[ocispec.AnnotationTitle])
-		if err := display.PrintSuccessorStatus(ctx, desc, "Skipped  ", store, committed, verbose); err != nil {
+		if err := display.PrintSuccessorStatus(ctx, desc, "Skipped  ", fetcher, committed, verbose); err != nil {
 			return err
 		}
 		return display.PrintStatus(desc, "Uploaded ", verbose)
