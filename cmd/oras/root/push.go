e5a7c2e1dcc8728678ff2843dbd1e012691a4a95
Billy Zha <jinzha1@microsoft.com>
2023-07-04 17:33:32 +0800

    fix: pushing based on temporary file store path (#996)
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- cmd/oras/root/push.go
+++ cmd/oras/root/push.go
@@ -27,9 +27,11 @@ import (
 	"oras.land/oras-go/v2"
 	"oras.land/oras-go/v2/content"
 	"oras.land/oras-go/v2/content/file"
+	"oras.land/oras-go/v2/content/memory"
 	"oras.land/oras/cmd/oras/internal/display"
 	"oras.land/oras/cmd/oras/internal/fileref"
 	"oras.land/oras/cmd/oras/internal/option"
+	"oras.land/oras/internal/contentutil"
 )
 
 type pushOptions struct {
@@ -159,12 +161,13 @@ func runPush(ctx context.Context, opts pushOptions) error {
 	if err != nil {
 		return err
 	}
+	memoryStore := memory.New()
 	pack := func() (ocispec.Descriptor, error) {
-		root, err := oras.Pack(ctx, store, opts.artifactType, descs, packOpts)
+		root, err := oras.Pack(ctx, memoryStore, opts.artifactType, descs, packOpts)
 		if err != nil {
 			return ocispec.Descriptor{}, err
 		}
-		if err = store.Tag(ctx, root, root.Digest.String()); err != nil {
+		if err = memoryStore.Tag(ctx, root, root.Digest.String()); err != nil {
 			return ocispec.Descriptor{}, err
 		}
 		return root, nil
@@ -177,12 +180,13 @@ func runPush(ctx context.Context, opts pushOptions) error {
 	}
 	copyOptions := oras.DefaultCopyOptions
 	copyOptions.Concurrency = opts.concurrency
-	updateDisplayOption(&copyOptions.CopyGraphOptions, store, opts.Verbose)
+	union := contentutil.MultiReadOnlyTarget(memoryStore, store)
+	updateDisplayOption(&copyOptions.CopyGraphOptions, union, opts.Verbose)
 	copy := func(root ocispec.Descriptor) error {
 		if tag := opts.Reference; tag == "" {
-			err = oras.CopyGraph(ctx, store, dst, root, copyOptions.CopyGraphOptions)
+			err = oras.CopyGraph(ctx, union, dst, root, copyOptions.CopyGraphOptions)
 		} else {
-			_, err = oras.Copy(ctx, store, root.Digest.String(), dst, tag, copyOptions)
+			_, err = oras.Copy(ctx, union, root.Digest.String(), dst, tag, copyOptions)
 		}
 		return err
 	}
@@ -195,7 +199,7 @@ func runPush(ctx context.Context, opts pushOptions) error {
 	fmt.Println("Pushed", opts.AnnotatedReference())
 
 	if len(opts.extraRefs) != 0 {
-		contentBytes, err := content.FetchAll(ctx, store, root)
+		contentBytes, err := content.FetchAll(ctx, memoryStore, root)
 		if err != nil {
 			return err
 		}
@@ -209,10 +213,10 @@ func runPush(ctx context.Context, opts pushOptions) error {
 	fmt.Println("Digest:", root.Digest)
 
 	// Export manifest
-	return opts.ExportManifest(ctx, store, root)
+	return opts.ExportManifest(ctx, memoryStore, root)
 }
 
-func updateDisplayOption(opts *oras.CopyGraphOptions, store content.Fetcher, verbose bool) {
+func updateDisplayOption(opts *oras.CopyGraphOptions, fetcher content.Fetcher, verbose bool) {
 	committed := &sync.Map{}
 	opts.PreCopy = display.StatusPrinter("Uploading", verbose)
 	opts.OnCopySkipped = func(ctx context.Context, desc ocispec.Descriptor) error {
@@ -221,7 +225,7 @@ func updateDisplayOption(opts *oras.CopyGraphOptions, store content.Fetcher, ver
 	}
 	opts.PostCopy = func(ctx context.Context, desc ocispec.Descriptor) error {
 		committed.Store(desc.Digest.String(), desc.Annotations[ocispec.AnnotationTitle])
-		if err := display.PrintSuccessorStatus(ctx, desc, "Skipped  ", store, committed, verbose); err != nil {
+		if err := display.PrintSuccessorStatus(ctx, desc, "Skipped  ", fetcher, committed, verbose); err != nil {
 			return err
 		}
 		return display.PrintStatus(desc, "Uploaded ", verbose)
