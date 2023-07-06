32bfb875d3e288f0c5133b81af2c742d257cbc9b
Sajay Antony <sajaya@microsoft.com>
2023-07-06 19:53:22 +0000

    version: bump for release of v1.1.0-rc1
    Signed-off-by: Sajay Antony <sajaya@microsoft.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -17,7 +17,7 @@ package version
 
 var (
 	// Version is the current version of the oras.
-	Version = "1.1.0-rc.1"
+	Version = "1.1.0-rc1"
 	// BuildMetadata is the extra build time data
 	BuildMetadata = "unreleased"
 	// GitCommit is the git sha1
