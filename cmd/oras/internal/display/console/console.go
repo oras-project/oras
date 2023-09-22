144e907364d618e401d79c2feeccbff39667ad67
Billy Zha <jinzha1@microsoft.com>
2023-09-22 08:47:11 +0000

    fix lint
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- cmd/oras/internal/display/console/console.go
+++ cmd/oras/internal/display/console/console.go
@@ -67,29 +67,29 @@ func New(f *os.File) (*Console, error) {
 
 // Save saves the current cursor position.
 func (c *Console) Save() {
-	c.Write([]byte(aec.Hide.Apply(Save)))
+	_, _ = c.Write([]byte(aec.Hide.Apply(Save)))
 }
 
 // NewRow allocates a horizontal space to the output area with scroll if needed.
 func (c *Console) NewRow() {
-	c.Write([]byte(Restore))
-	c.Write([]byte("\n"))
-	c.Write([]byte(Save))
+	_, _ = c.Write([]byte(Restore))
+	_, _ = c.Write([]byte("\n"))
+	_, _ = c.Write([]byte(Save))
 }
 
 // OutputTo outputs a string to a specific line.
 func (c *Console) OutputTo(upCnt uint, str string) {
-	c.Write([]byte(Restore))
-	c.Write([]byte(aec.PreviousLine(upCnt).Apply(str)))
-	c.Write([]byte(" "))
-	c.Write([]byte(aec.EraseLine(aec.EraseModes.Tail).String()))
+	_, _ = c.Write([]byte(Restore))
+	_, _ = c.Write([]byte(aec.PreviousLine(upCnt).Apply(str)))
+	_, _ = c.Write([]byte(" "))
+	_, _ = c.Write([]byte(aec.EraseLine(aec.EraseModes.Tail).String()))
 }
 
 // Restore restores the saved cursor position.
 func (c *Console) Restore() {
 	// cannot use aec.Restore since DEC has better compatilibity than SCO
-	c.Write([]byte(Restore))
-	c.Write([]byte(aec.Column(0).
+	_, _ = c.Write([]byte(Restore))
+	_, _ = c.Write([]byte(aec.Column(0).
 		With(aec.EraseLine(aec.EraseModes.All)).
 		With(aec.Show).String()))
 }
