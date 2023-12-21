e905e356d8963cac7e3fbebde375817af5a091ed
Billy Zha <jinzha1@microsoft.com>
2023-12-21 07:11:34 +0000

    bug fix
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- cmd/oras/root/manifest/push.go
+++ cmd/oras/root/manifest/push.go
@@ -82,7 +82,7 @@ Example - Push a manifest to repository 'localhost:5000/hello' and tag with 'tag
 Example - Push a manifest to an OCI image layout folder 'layout-dir' and tag with 'v1':
   oras manifest push --oci-layout layout-dir:v1 manifest.json
 `,
-		Args: oerrors.CheckArgs(argument.Exactly(1), "the destination to push to and the file to read manifest content from"),
+		Args: oerrors.CheckArgs(argument.Exactly(2), "the destination to push to and the file to read manifest content from"),
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			opts.fileRef = args[1]
 			if opts.fileRef == "-" && opts.PasswordFromStdin {
