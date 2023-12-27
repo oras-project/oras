dbe605be7f50b0c0a9b03b67f6eb4cbea55680a6
Billy Zha <jinzha1@microsoft.com>
2023-12-27 07:06:58 +0000

    chore(ux): improve error output for unknown spec flag
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- cmd/oras/internal/option/spec.go
+++ cmd/oras/internal/option/spec.go
@@ -20,11 +20,14 @@ import (
 
 	"github.com/spf13/pflag"
 	"oras.land/oras-go/v2"
+	oerrors "oras.land/oras/cmd/oras/internal/errors"
 )
 
 const (
-	ImageSpecV1_1 = "v1.1"
-	ImageSpecV1_0 = "v1.0"
+	ImageSpecV1_1    = "v1.1"
+	ImageSpecV1_0    = "v1.0"
+	ReferrersTagV1_1 = "v1.1-referrers-tag"
+	ReferrersAPIV1_1 = "v1.1-referrers-api"
 )
 
 // ImageSpec option struct.
@@ -33,22 +36,37 @@ type ImageSpec struct {
 	PackVersion oras.PackManifestVersion
 }
 
-// Parse parses flags into the option.
-func (opts *ImageSpec) Parse() error {
-	switch opts.flag {
-	case ImageSpecV1_1:
-		opts.PackVersion = oras.PackManifestVersion1_1_RC4
+// Set validates and sets the flag value from a string argument.
+func (i *ImageSpec) Set(value string) error {
+	i.flag = value
+	switch value {
+	case ImageSpecV1_1, "":
+		// default to v1.1
+		i.PackVersion = oras.PackManifestVersion1_1_RC4
 	case ImageSpecV1_0:
-		opts.PackVersion = oras.PackManifestVersion1_0
+		i.PackVersion = oras.PackManifestVersion1_0
 	default:
-		return fmt.Errorf("unknown image specification flag: %q", opts.flag)
+		return &oerrors.Error{
+			Err:            fmt.Errorf("unknown image specification flag: %s", value),
+			Recommendation: fmt.Sprintf("Available options: %s", i.Type()),
+		}
 	}
 	return nil
 }
 
+// Type returns the type of the flag.
+func (i *ImageSpec) Type() string {
+	return fmt.Sprintf("%s, %s", ImageSpecV1_1, ImageSpecV1_0)
+}
+
+// String returns the string representation of the flag.
+func (i *ImageSpec) String() string {
+	return i.flag
+}
+
 // ApplyFlags applies flags to a command flag set.
 func (opts *ImageSpec) ApplyFlags(fs *pflag.FlagSet) {
-	fs.StringVar(&opts.flag, "image-spec", ImageSpecV1_1, fmt.Sprintf("[Experimental] specify manifest type for building artifact. options: %s, %s", ImageSpecV1_1, ImageSpecV1_0))
+	fs.Var(opts, "image-spec", "[Experimental] specify manifest type for building artifact")
 }
 
 // DistributionSpec option struct.
@@ -58,28 +76,43 @@ type DistributionSpec struct {
 	ReferrersAPI *bool
 
 	// specFlag should be provided in form of`<version>-<api>-<option>`
-	specFlag string
+	flag string
 }
 
-// Parse parses flags into the option.
-func (opts *DistributionSpec) Parse() error {
-	switch opts.specFlag {
+// Set validates and sets the flag value from a string argument.
+func (d *DistributionSpec) Set(value string) error {
+	d.flag = value
+	switch d.flag {
 	case "":
-		opts.ReferrersAPI = nil
-	case "v1.1-referrers-tag":
+		// default to unknown
+		d.ReferrersAPI = nil
+	case ReferrersTagV1_1:
 		isApi := false
-		opts.ReferrersAPI = &isApi
-	case "v1.1-referrers-api":
+		d.ReferrersAPI = &isApi
+	case ReferrersAPIV1_1:
 		isApi := true
-		opts.ReferrersAPI = &isApi
+		d.ReferrersAPI = &isApi
 	default:
-		return fmt.Errorf("unknown distribution specification flag: %q", opts.specFlag)
+		return &oerrors.Error{
+			Err:            fmt.Errorf("unknown distribution specification flag: %s", value),
+			Recommendation: fmt.Sprintf("Available options: %s", d.Type()),
+		}
 	}
 	return nil
+}
+
+// Type returns the type of the flag.
+func (d *DistributionSpec) Type() string {
+	return fmt.Sprintf("%s, %s", ReferrersTagV1_1, ReferrersAPIV1_1)
+}
+
+// String returns the string representation of the flag.
+func (d *DistributionSpec) String() string {
+	return d.flag
 }
 
 // ApplyFlagsWithPrefix applies flags to a command flag set with a prefix string.
 func (opts *DistributionSpec) ApplyFlagsWithPrefix(fs *pflag.FlagSet, prefix, description string) {
 	flagPrefix, notePrefix := applyPrefix(prefix, description)
-	fs.StringVar(&opts.specFlag, flagPrefix+"distribution-spec", "", "[Preview] set OCI distribution spec version and API option for "+notePrefix+"target. options: v1.1-referrers-api, v1.1-referrers-tag")
+	fs.Var(opts, flagPrefix+"distribution-spec", "[Preview] set OCI distribution spec version and API option for "+notePrefix+"target. options: v1.1-referrers-api, v1.1-referrers-tag")
 }
