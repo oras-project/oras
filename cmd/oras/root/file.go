c3aabe73cc18af544998ef36292e801bef2494a9
Billy Zha <jinzha1@microsoft.com>
2024-01-31 10:03:44 +0000

    add operation code
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- cmd/oras/root/file.go
+++ cmd/oras/root/file.go
@@ -70,7 +70,7 @@ func addFile(ctx context.Context, store *file.Store, name string, mediaType stri
 	if err != nil {
 		var pathErr *fs.PathError
 		if errors.As(err, &pathErr) {
-			err = fmt.Errorf("%s: %w", pathErr.Path, pathErr.Err)
+			err = fmt.Errorf("%s %s: %w", pathErr.Op, pathErr.Path, pathErr.Err)
 		}
 		return ocispec.Descriptor{}, err
 	}
