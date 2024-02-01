94b7a6f225f5a9046cf94287695e53c4e9bc58cd
Billy Zha <jinzha1@microsoft.com>
2024-02-01 10:11:32 +0000

    add license header
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- cmd/oras/internal/display/metadata/json/attach.go
+++ cmd/oras/internal/display/metadata/json/attach.go
@@ -1,3 +1,18 @@
+/*
+Copyright The ORAS Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package json
 
 import (
@@ -7,12 +22,15 @@ import (
 	"oras.land/oras/cmd/oras/internal/option"
 )
 
+// AttachHandler handles metadata events for attach command.
 type AttachHandler struct{}
 
+// NewAttachHandler returns a new hander for attach metadata events.
 func NewAttachHandler() metadata.AttachHandler {
 	return AttachHandler{}
 }
 
+// OnCompleted is called when the attach operation is completed.
 func (AttachHandler) OnCompleted(opts *option.Target, root, subject ocispec.Descriptor) error {
 	return printJSON(model.NewPush(root, opts.Path))
 }
