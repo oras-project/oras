b605fe5c8696c612d192df60f80bfa86629169db
Billy Zha <jinzha1@microsoft.com>
2024-02-01 11:24:07 +0000

    add coverage
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

---
+++ cmd/oras/internal/display/status/tty_test.go
@@ -0,0 +1,35 @@
+/*
+Copyright The ORAS Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package status
+
+import (
+	"os"
+	"testing"
+)
+
+func TestTTYPushHandler_OnFileLoading(t *testing.T) {
+	ph := NewTTYPushHandler(os.Stdout)
+	if ph.OnFileLoading("test") != nil {
+		t.Error("OnFileLoading() should not return an error")
+	}
+}
+
+func TestTTYPushHandler_OnEmptyArtifact(t *testing.T) {
+	ph := NewTTYAttachHandler(os.Stdout)
+	if ph.OnEmptyArtifact() != nil {
+		t.Error("OnEmptyArtifact() should not return an error")
+	}
+}
