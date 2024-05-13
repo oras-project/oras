364729f64f2a2cac9de138fe5a1f0f7c38449e65
Billy Zha <jinzha1@microsoft.com>
2024-05-13 14:30:08 +0000

    code clean
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- cmd/oras/internal/errors/errors.go
+++ cmd/oras/internal/errors/errors.go
@@ -32,6 +32,7 @@ const RegistryErrorPrefix = "Error response from registry:"
 // UnsupportedFormatTypeError generates the error message for an invalid type.
 type UnsupportedFormatTypeError string
 
+// Error implements the error interface.
 func (e UnsupportedFormatTypeError) Error() string {
 	return "unsupported format type: " + string(e)
 }
