724f56b01cc82e4aed52df974ea4eed20f056f4f
Xiaoxuan Wang <103478229+wangxiaoxuan273@users.noreply.github.com>
2025-02-19 16:55:12 +0800

    refactor: improve metadata handler of oras cp (#1624)
    Signed-off-by: Xiaoxuan Wang <xiaoxuanwang@microsoft.com>

--- cmd/oras/internal/display/metadata/interface.go
+++ cmd/oras/internal/display/metadata/interface.go
@@ -104,4 +104,7 @@ type ManifestIndexUpdateHandler ManifestIndexCreateHandler
 // CopyHandler handles metadata output for cp events.
 type CopyHandler interface {
 	TaggedHandler
+	Renderer
+
+	OnCopied(target *option.BinaryTarget, desc ocispec.Descriptor) error
 }
