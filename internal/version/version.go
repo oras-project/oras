87379d22f42b5132ed583587766f0a780f9bd322
Lixia (Sylvia) Lei <lixlei@microsoft.com>
2025-02-25 16:30:27 +0800

    bump: tag and release ORAS CLI v1.3.0-beta.2
    Signed-off-by: Lixia (Sylvia) Lei <lixlei@microsoft.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -17,7 +17,7 @@ package version
 
 var (
 	// Version is the current version of the oras.
-	Version = "1.3.0-beta.1"
+	Version = "1.3.0-beta.2"
 	// BuildMetadata is the extra build time data
 	BuildMetadata = "unreleased"
 	// GitCommit is the git sha1
