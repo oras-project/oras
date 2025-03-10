7df88942fee6536960d3101c718e43b25b18b150
Xiaoxuan Wang <xiaoxuanwang@microsoft.com>
2025-03-10 11:17:08 +0800

    fix failed e2e tests
    Signed-off-by: Xiaoxuan Wang <xiaoxuanwang@microsoft.com>

--- test/e2e/suite/command/cp.go
+++ test/e2e/suite/command/cp.go
@@ -223,7 +223,7 @@ var _ = Describe("1.1 registry users:", func() {
 			// validate
 			CompareRef(RegistryRef(ZOTHost, ImageRepo, ma.Digest), dst)
 
-			digests := ORAS("discover", dst, "--format", "go-template={{range .manifests}}{{println .digest}}{{end}}").Exec().Out.Contents()
+			digests := ORAS("discover", dst, "--format", "go-template={{range .referrers}}{{println .digest}}{{end}}").Exec().Out.Contents()
 			for _, digest := range strings.Split(strings.TrimSpace(string(digests)), "\n") {
 				CompareRef(RegistryRef(ZOTHost, ArtifactRepo, digest), RegistryRef(ZOTHost, dstRepo, digest))
 			}
@@ -241,7 +241,7 @@ var _ = Describe("1.1 registry users:", func() {
 				Exec()
 			// validate
 			CompareRef(RegistryRef(ZOTHost, ImageRepo, ma.Digest), dst)
-			digests := ORAS("discover", dst, "--format", "go-template={{range .manifests}}{{println .digest}}{{end}}").Exec().Out.Contents()
+			digests := ORAS("discover", dst, "--format", "go-template={{range .referrers}}{{println .digest}}{{end}}").Exec().Out.Contents()
 			for _, digest := range strings.Split(strings.TrimSpace(string(digests)), "\n") {
 				CompareRef(RegistryRef(ZOTHost, ArtifactRepo, digest), RegistryRef(ZOTHost, dstRepo, digest))
 			}
@@ -268,7 +268,7 @@ var _ = Describe("1.1 registry users:", func() {
 				Exec()
 			// validate
 			CompareRef(RegistryRef(ZOTHost, ImageRepo, ma.Digest), dst)
-			digests := ORAS("discover", dst, "--format", "go-template={{range .manifests}}{{println .digest}}{{end}}").Exec().Out.Contents()
+			digests := ORAS("discover", dst, "--format", "go-template={{range .referrers}}{{println .digest}}{{end}}").Exec().Out.Contents()
 			for _, digest := range strings.Split(strings.TrimSpace(string(digests)), "\n") {
 				CompareRef(RegistryRef(ZOTHost, ArtifactRepo, digest), RegistryRef(ZOTHost, dstRepo, digest))
 			}
@@ -308,7 +308,7 @@ var _ = Describe("1.1 registry users:", func() {
 				Exec()
 			// validate
 			CompareRef(RegistryRef(ZOTHost, ArtifactRepo, digest), dst)
-			digests := ORAS("discover", dst, "--format", "go-template={{range .manifests}}{{println .digest}}{{end}}").Exec().Out.Contents()
+			digests := ORAS("discover", dst, "--format", "go-template={{range .referrers}}{{println .digest}}{{end}}").Exec().Out.Contents()
 			for _, digest := range strings.Split(strings.TrimSpace(string(digests)), "\n") {
 				CompareRef(RegistryRef(ZOTHost, ArtifactRepo, digest), RegistryRef(ZOTHost, dstRepo, digest))
 			}
@@ -326,7 +326,7 @@ var _ = Describe("1.1 registry users:", func() {
 				Exec()
 			// validate
 			CompareRef(RegistryRef(ZOTHost, ArtifactRepo, digest), RegistryRef(ZOTHost, dstRepo, digest))
-			digests := ORAS("discover", RegistryRef(ZOTHost, dstRepo, digest), "--format", "go-template={{range .manifests}}{{println .digest}}{{end}}").Exec().Out.Contents()
+			digests := ORAS("discover", RegistryRef(ZOTHost, dstRepo, digest), "--format", "go-template={{range .referrers}}{{println .digest}}{{end}}").Exec().Out.Contents()
 			for _, digest := range strings.Split(strings.TrimSpace(string(digests)), "\n") {
 				CompareRef(RegistryRef(ZOTHost, ArtifactRepo, digest), RegistryRef(ZOTHost, dstRepo, digest))
 			}
--- test/e2e/suite/command/discover.go
+++ test/e2e/suite/command/discover.go
@@ -209,7 +209,7 @@ var _ = Describe("1.1 registry users:", func() {
 	})
 	When("running discover command with go-template output", func() {
 		It("should show referrers digest of a subject", func() {
-			ORAS("discover", subjectRef, "--format", "go-template={{(first .manifests).reference}}").
+			ORAS("discover", subjectRef, "--format", "go-template={{(first .referrers).reference}}").
 				MatchContent(RegistryRef(ZOTHost, ArtifactRepo, foobar.SBOMImageReferrer.Digest.String())).
 				Exec()
 		})
