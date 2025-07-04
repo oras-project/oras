09754b9dda74d81550d95e2b26f2785d586dc86e
Lixia (Sylvia) Lei <lixlei@microsoft.com>
2025-07-04 17:16:37 +0800

    clean up
    Signed-off-by: Lixia (Sylvia) Lei <lixlei@microsoft.com>

--- cmd/oras/internal/option/remote.go
+++ cmd/oras/internal/option/remote.go
@@ -407,10 +407,9 @@ func (remo *Remote) Modify(cmd *cobra.Command, err error) (error, bool) {
 	}
 
 	if errors.As(err, &errResp) {
-		// TODO: handle this
 		cmd.SetErrPrefix(oerrors.RegistryErrorPrefix)
 		return &oerrors.Error{
-			Err: oerrors.TrimErrResp(err, errResp),
+			Err: oerrors.ReportErrResp(errResp),
 		}, true
 	}
 	return err, false
