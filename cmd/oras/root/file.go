7cbdb366da6d0f603fba750fba3f0be04cfbb145
Rafael Gieschke <rafael@gieschke.de>
2025-10-20 09:07:18 +0200

    fix: allow setting org.opencontainers.image.title in annotation file (#1872)
    Signed-off-by: Rafael Gieschke <rafael@gieschke.de>

--- cmd/oras/root/file.go
+++ cmd/oras/root/file.go
@@ -42,6 +42,12 @@ func loadFiles(ctx context.Context, store *file.Store, annotations map[string]ma
 			name = filepath.ToSlash(name)
 		}
 
+		if value, ok := annotations[filename]; ok {
+			if nameFromAnnotations, ok := value[ocispec.AnnotationTitle]; ok {
+				name = nameFromAnnotations
+			}
+		}
+
 		err = displayStatus.OnFileLoading(name)
 		if err != nil {
 			return nil, err
